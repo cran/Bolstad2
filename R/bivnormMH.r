b638a415595cf12b94a6d931fa3c87b63d4e5d9a
James M. Curran <curran@stat.auckland.ac.nz>
2010-01-25 00:00:00 +0000

    version 1.0-26

--- R/bivnormMH.r
+++ R/bivnormMH.r
@@ -7,262 +7,260 @@ bivnormMH<-function(rho, rho1 = 0.9, sigma = c(1.2, 1.2), steps = 1000, type = '
     if(steps<100)
         warning("You should really do more than 100 steps")
 
-    targetSample.df<-data.frame(x=rep(0,steps),y=rep(0,steps))
-    candidate.df<-data.frame(x=rep(0,steps),y=rep(0,steps))
+    target = candidate = matrix(0, nc = 2, nr = steps)
 
     if(length(grep('^[Rr]',type))>0){
-        type <- 'rw'
+        type = 'rw'
     }else if(length(grep('^[Ii]',type))>0){
-        type <- 'ind'
+        type = 'ind'
     }else if(length(grep('^[Bb]',type))>0){
-        type <- 'block'
+        type = 'block'
     }else if(length(grep('^[Gg]',type))>0){
-        type <- 'gibbs'
+        type = 'gibbs'
     }else{
         stop("Type must be one of rw, ind, block or gibbs")
     }
 
-    x0<-c(0,0)
-    x1<-c(0,0)
-    mu<-c(0,0)
+    x0 = c(0,0)
+    x1 = c(0,0)
+    mu = c(0,0)
 
-    if(type=='rw'){
+    if(type == 'rw'){
 
-        startValue<-c(2,2)
+        startValue = c(2,2)
 
-        sigma1<-0.5
-        var1<-sigma1^2
+        sigma1 = 0.5
+        var1 = sigma1^2
 
-        sigma2<-1
-        var2<-sigma2^2
+        sigma2 = 1
+        var2 = sigma2^2
 
-        k<-2*pi/1000
+        k = 2*pi/1000
 
-        u<-runif(steps)
+        u = runif(steps)
 
-        z1<-rnorm(steps,0,sigma1)
-        z2<-rnorm(steps,0,sigma1)
+        z1 = rnorm(steps,0,sigma1)
+        z2 = rnorm(steps,0,sigma1)
 
-        w<-1-rho^2
+        w = 1-rho^2
 
-        targetSample.df[1,]<-startValue
+        target[1,] = startValue
 
-        x1 <- targetSample.df[1,]
-        mu <- targetSample.df[1,]
-        x0 <- c(mu[1]+z1[1], mu[2]+z2[1])
+        x1 = target[1,]
+        mu = target[1,]
+        x0 = c(mu[1]+z1[1], mu[2]+z2[1])
 
-        candidate.df$x <- rep(0, steps)
-        candidate.df$y <- rep(0, steps)
-
-        candidate.df[2,] <- x0
+        candidate[2,] = x0
 
         for(n in 2:steps){
-            n1 <- n-1
+            n1 = n-1
 
-            x1<-targetSample.df[n1,]
-            x0<-candidate.df[n,]
+            x1 = target[n1,]
+            x0 = candidate[n,]
 
 
-            canDens <- exp(-1/(2*var2*w)*(x0[1]^2-2*rho*x0[1]*x0[2] +x0[2]^2))
-            curDens <- exp(-1/(2*var2*w)*(x1[1]^2-2*rho*x1[1]*x1[2] +x1[2]^2))
+            canDens = exp(-1/(2*var2*w)*(x0[1]^2-2*rho*x0[1]*x0[2] +x0[2]^2))
+            curDens = exp(-1/(2*var2*w)*(x1[1]^2-2*rho*x1[1]*x1[2] +x1[2]^2))
 
 
             if(u[n] < canDens/curDens){ ## candidate accepted
-                targetSample.df[n,] <- x0
+                target[n,] = x0
             }else{## candidate rejected
-                targetSample.df[n,] <- targetSample.df[n1,]
+                target[n,] = target[n1,]
             }
 
-            mu <- targetSample.df[n,]
-            x0 <- c(mu[1]+z1[n], mu[2]+z2[n])
-            candidate.df[n+1,] <- x0
+            mu = target[n,]
+            x0 = c(mu[1]+z1[n], mu[2]+z2[n])
+
+            if(n<steps)
+                candidate[n+1,] = x0
 
         }
     }else if(type == 'ind'){
         if(rho1 < -1 | rho > 1)
             stop("rho1 must be between -1 and 1")
 
-        if(any(sigma<=0))
+        if(any(sigma<= 0))
             stop("The elements of sigma must be strictly positive non-zero")
 
-        u<-runif(steps)
+        u = runif(steps)
 
-        startValue <- c(2.0, 1.5)
-        x1 <- startValue
-        x0 <- matrix(rnorm(2*steps, rep(0,2*steps), rep(sigma,steps)),nc=2)
+        startValue = c(2.0, 1.5)
+        x1 = startValue
+        x0 = matrix(rnorm(2*steps, rep(0,2*steps), rep(sigma,steps)),nc = 2)
 
-        x0[,2]<-rho1*x0[,1]+sqrt(1-rho1^2)*x0[,2]
+        x0[,2] = rho1*x0[,1]+sqrt(1-rho1^2)*x0[,2]
 
-        gDensInd<-function(x, rho){
+        gDensInd = function(x, rho){
 
-            y<-x[2]
-            x<-x[1]
+            y = x[2]
+            x = x[1]
 
             return(exp(-0.5/(1-rho^2)*(x^2 - 2*rho*x*y + y^2)))
         }
 
-        qDens<-function(x, rho, rho1, sigma){
-            zy<-x[2]/sigma[2]
-            zx<-x[1]/sigma[1]
+        qDens = function(x, rho, rho1, sigma){
+            zy = x[2]/sigma[2]
+            zx = x[1]/sigma[1]
 
             return(exp(-0.5/(1-rho1^2)*(zx^2 - 2*rho*zx*zy + zy^2)))
         }
 
         for(n in 1:steps){
-            targetSample.df[n,] <- x1
-            candidate.df[n,] <- x0[n,]
+            target[n,] = x1
+            candidate[n,] = x0[n,]
 
-            cand <- gDensInd(x0[n,], rho)
-            cur <- gDensInd(x1, rho)
+            cand = gDensInd(x0[n,], rho)
+            cur = gDensInd(x1, rho)
 
-            qcand <- qDens(x0[n,], rho, rho1, sigma)
-            qcur <- qDens(x1, rho, rho1, sigma)
+            qcand = qDens(x0[n,], rho, rho1, sigma)
+            qcur = qDens(x1, rho, rho1, sigma)
 
-            ratio <- (cand/cur)*(qcur/qcand)
+            ratio = (cand/cur)*(qcur/qcand)
 
             if(u[n] < ratio)
-                x1 <- x0[n,]
+                x1 = x0[n,]
 
         }
     }else if(type=='block'){
-        twosteps <- 2*steps
-        targetSample.df <- data.frame(x = rep(0, twosteps),
-                                      y = rep(0, twosteps))
-        candidate.df <- targetSample.df
-        u<-runif(twosteps)
+        twosteps = 2*steps
+        target = candidate = matrix(0, nc = 2, nr = twosteps)
 
-        startValue <- c(2, 1.5)
-        x1<-startValue
+        u = runif(twosteps)
 
-        sx<-sqrt(1-rho^2)
-        vx<-sx^2
+        startValue = c(2, 1.5)
+        x1 = startValue
 
-        sigma1<-0.75
-        var1<-sigma1^2
+        sx = sqrt(1-rho^2)
+        vx = sx^2
 
-        gDensBlock<-function(x, mx, vx){
+        sigma1 = 0.75
+        var1 = sigma1^2
+
+        gDensBlock = function(x, mx, vx){
             return(exp(-0.5*(x-mx)^2/vx))
         }
 
         for(n in 1:steps){
             ## draw from Block 1
-            mx <- rho*x1[2]
+            mx = rho*x1[2]
 
             ## draw candidate
 
-            x0<-c(rnorm(1,mx,sigma1), x1[2])
+            x0 = c(rnorm(1,mx,sigma1), x1[2])
 
-            n1<-2*n-1
+            n1 = 2*n-1
 
-            targetSample.df[n1,]<-x1
-            candidate.df[n1,]<-x0
+            target[n1,] = x1
+            candidate[n1,] = x0
 
-            cand <- gDensBlock(x0[1], mx, vx)
-            cur <- gDensBlock(x1[1], mx, vx)
+            cand = gDensBlock(x0[1], mx, vx)
+            cur = gDensBlock(x1[1], mx, vx)
 
-            qcand <- gDensBlock(x0[1], mx, var1)
-            qcur <- gDensBlock(x1[1], mx, var1)
+            qcand = gDensBlock(x0[1], mx, var1)
+            qcur = gDensBlock(x1[1], mx, var1)
 
-            ratio<-(cand/cur)*(qcur/qcand)
+            ratio = (cand/cur)*(qcur/qcand)
 
             if(u[n1] < ratio)
-                x1<-x0
+                x1 = x0
 
             ## draw from block 2
 
-            my<-rho*x1[1]
+            my = rho*x1[1]
 
             ## draw candidate
 
-            x0 <- c(x1[1], rnorm(1, my, sigma1))
+            x0 = c(x1[1], rnorm(1, my, sigma1))
 
-            n2<-2*n
+            n2 = 2*n
 
-            targetSample.df[n2,]<-x1
-            candidate.df[n2,]<-x0
+            target[n2,] = x1
+            candidate[n2,] = x0
 
-            cand <- gDensBlock(x0[2], my, vx)
-            cur <- gDensBlock(x1[2], my, vx)
+            cand = gDensBlock(x0[2], my, vx)
+            cur = gDensBlock(x1[2], my, vx)
 
-            qcand <- gDensBlock(x0[2], my, var1)
-            qcur <- gDensBlock(x1[2], my, var1)
+            qcand = gDensBlock(x0[2], my, var1)
+            qcur = gDensBlock(x1[2], my, var1)
 
-            ratio<-(cand/cur)*(qcur/qcand)
+            ratio = (cand/cur)*(qcur/qcand)
 
             if(u[n2] < ratio)
-                x1<-x0
+                x1 = x0
         }
-    }else if(type=='gibbs'){
-        twosteps <- 2*steps
-        targetSample.df <- data.frame(x = rep(0, twosteps),
-                                      y = rep(0, twosteps))
-        candidate.df <- targetSample.df
-        u<-runif(twosteps)
+    }else if(type == 'gibbs'){
+        twosteps = 2*steps
+        target = candidate = matrix(0, nc = 2, nr = twosteps)
 
-        startValue <- c(2, 1.5)
-        x1 <- startValue
+        u = runif(twosteps)
 
-        sx<-sqrt(1-rho^2)
-        mx<-rho*x1[1]
-        vx<-sx^2
+        startValue = c(2, 1.5)
+        x1 = startValue
 
-        sigma1<-sx
-        var1<-sigma1^2
+        sx = sqrt(1-rho^2)
+        mx = rho*x1[1]
+        vx = sx^2
 
+        sigma1 = sx
+        var1 = sigma1^2
 
 
-        gDensGibbs <- function(x, mx, vx){
+
+        gDensGibbs = function(x, mx, vx){
             return(exp(-0.5*(x-mx)^2/vx))
         }
 
         for(n in 1:steps){
 
-            mx<-rho*x1[1]
+            mx = rho*x1[1]
 
             ## draw a candidate
-            x0 <- c(rnorm(1, mx, sigma1), x1[2])
+            x0 = c(rnorm(1, mx, sigma1), x1[2])
 
-            n1<-2*n-1
+            n1 = 2*n-1
 
-            targetSample.df[n1, ] <- x1
-            candidate.df[n1,] <- x0
+            target[n1, ] = x1
+            candidate[n1,] = x0
 
-            cand <- gDensGibbs(x0[1], mx, vx)
-            cur <- gDensGibbs(x1[1], mx ,vx)
+            cand = gDensGibbs(x0[1], mx, vx)
+            cur = gDensGibbs(x1[1], mx ,vx)
 
-            qcand <- gDensGibbs(x0[1], mx, var1)
-            qcur <- gDensGibbs(x1[1], mx ,var1)
+            qcand = gDensGibbs(x0[1], mx, var1)
+            qcur = gDensGibbs(x1[1], mx ,var1)
 
-            ratio <- (cand/cur)*(qcur/qcand)
+            ratio = (cand/cur)*(qcur/qcand)
 
-            if(u[n1]<-ratio)
-                x1 <- x0
+            if(u[n1] < ratio)
+                x1 = x0
 
             ## draw a candidate
 
-            mx <- rho * x1[1]
-            x0 <- c(x1[1], rnorm(1, mx, sigma1))
+            mx = rho * x1[1]
+            x0 = c(x1[1], rnorm(1, mx, sigma1))
 
-            n2<-2*n
+            n2 = 2*n
 
-            targetSample.df[n2,] <- x1
-            candidate.df[n2,] <- x0
+            target[n2,] = x1
+            candidate[n2,] = x0
 
-            cand <- gDensGibbs(x0[2], mx, vx)
-            cur <- gDensGibbs(x1[2], mx, vx)
+            cand = gDensGibbs(x0[2], mx, vx)
+            cur = gDensGibbs(x1[2], mx, vx)
 
-            qcand <- gDensGibbs(x0[2], mx, var1)
-            qcur <- gDensGibbs(x1[2], mx, var1)
+            qcand = gDensGibbs(x0[2], mx, var1)
+            qcur = gDensGibbs(x1[2], mx, var1)
 
-            ratio <- (cand/cur)*(qcur/qcand)
+            ratio = (cand/cur)*(qcur/qcand)
 
-            if(u[n2]<ratio)
-                x1 <- x0
+            if(u[n2] < ratio)
+                x1 = x0
 
         }
     }
 
-    plot(y~x,data = targetSample.df,type="l")
-    invisible(list(targetSample = targetSample.df))
+    target = data.frame(x = target[,1], y = target[,2])
+
+    plot(y~x,data = target,type = "l")
+    invisible(list(targetSample = target))
 }
 
--- R/normGibbs.r
+++ R/normGibbs.r
@@ -1,14 +1,14 @@
-normGibbs<-function(y, steps = 1000, type = 'ind', ...){
+normGibbs = function(y, steps = 1000, type = 'ind', ...){
 
     if(length(grep('[Ii]',type))>0){
-        type <- 'ind'
+        type = 'ind'
     }else if(length(grep('[Jj]',type))>0){
-        type <- 'joint'
+        type = 'joint'
     }else{
         stop("Type must be ind or joint")
     }
 
-    dots <- list(...)
+    dots = list(...)
 
 
     if(type == 'ind'){
@@ -16,111 +16,94 @@ normGibbs<-function(y, steps = 1000, type = 'ind', ...){
         ## The dots can carry priorMu which, if specified
         ## can be a single number m0 or a vector m0, n0
 
-        m0 <- 0
-        n0 <- 0
-        muSpecified <- FALSE
+        m0 = 0
+        n0 = 0
+        muSpecified = FALSE
 
         if("priorMu" %in% names(dots)){
             if(length(dots$priorMu)==2){
-                m0 <- dots$priorMu[1]
-                n0 <- dots$priorMu[2]
-                muSpecified <- TRUE
+                m0 = dots$priorMu[1]
+                n0 = dots$priorMu[2]
+                muSpecified = TRUE
             }else{
-                m0 <- dots$priorMu
-                muSpecified <- TRUE
+                m0 = dots$priorMu
+                muSpecified = TRUE
             }
         }
 
         ## The dots can carry priorVar which, if specified
         ## must be a vector of length 2
 
-        s0 <- 0
-        kappa0 <- 0
-        nObs <- length(y)
-        yBar <- mean(y)
-        SSy <- sum((y-yBar)^2)
+        s0 = 0
+        kappa0 = 0
+        nObs = length(y)
+        yBar = mean(y)
+        SSy = sum((y-yBar)^2)
 
-        varSpecified <- FALSE
+        varSpecified = FALSE
 
         if("priorVar" %in% names(dots)){
             if(length(dots$priorVar)!=2)
                 stop("priorVar must have two elements, s0 and kappa0")
             else{
-                s0 <- dots$priorVar[1]
-                kappa0 <-dots$priorVar[2]
-                varSpecified <- TRUE
+                s0 = dots$priorVar[1]
+                kappa0 =dots$priorVar[2]
+                varSpecified = TRUE
             }
         }
 
-        v0 <- ifelse(varSpecified, s0/rchisq(1,df = kappa0), SSy/(nObs-1))
-        kappa1 <- kappa0 + nObs
+        v0 = ifelse(varSpecified, s0/rchisq(1,df = kappa0), SSy/(nObs-1))
+        kappa1 = kappa0 + nObs
 
-        prec0 <- 0
-        mu0 <- yBar
+        prec0 = 0
+        mu0 = yBar
 
         if(muSpecified){
-            prec0 <- n0/v0
-            mu0 <- m0 + rnorm(1)*sqrt(v0)
+            prec0 = n0/v0
+            mu0 = m0 + rnorm(1)*sqrt(v0)
         }else{
-            prec0 <- 0
-            m0 <- 0
-            mu0 <- yBar
+            prec0 = 0
+            m0 = 0
+            mu0 = yBar
         }
 
-        SSt <- sum((y-mu0)^2)
-        s1 <- s0 + SSt
+        SSt = sum((y-mu0)^2)
+        s1 = s0 + SSt
 
-        Chi <- rchisq(steps, df = kappa1)
-        z <- rnorm(steps)
+        Chi = rchisq(steps, df = kappa1)
+        z = rnorm(steps)
 
-        varSample <- c(s1/Chi[1],rep(0, steps-1))
+        varSample = c(s1/Chi[1],rep(0,steps-1))
 
 
-        prec0 <- ifelse(muSpecified, n0/varSample[1], 0)
+        prec0 = ifelse(muSpecified, n0/varSample[1], 0)
 
-        precData <- nObs / varSample[1]
-        prec1 <- prec0 + precData
-        v1 <- 1/prec1
-        m1 <- m0*prec0/prec1 + yBar*precData/prec1
+        precData = nObs / varSample[1]
+        prec1 = prec0 + precData
+        v1 = 1/prec1
+        m1 = m0*prec0/prec1 + yBar*precData/prec1
 
-        temp.df<-data.frame(SSt = rep(0, steps), s1 = rep(0, steps),
-                            kappa1 = rep(0, steps), prec = rep(0, steps),
-                            v = rep(0, steps), std = rep(0, steps),
-                            mean = rep(0, steps))
-
-        muSample <- rep(0, steps)
-        muSample[1] <- z[1]*sqrt(v1)+m1
-
-        temp.df[1,]<-c(SSt, s1, kappa1, prec1, v1, sqrt(v1), m1)
+        muSample = rep(0, steps)
+        muSample[1] = z[1]*sqrt(v1)+m1
 
         for(i in 2:steps){
-            SSt <- sum((y-muSample[i-1])^2)
-            s1 <- s0 + SSt
-            varSample[i] <- s1/Chi[i]
+            SSt = sum((y-muSample[i-1])^2)
+            s1 = s0 + SSt
+            varSample[i] = s1/Chi[i]
 
-            temp.df$SSt[i] <- SSt
-            temp.df$s1[i] <- s1
-            temp.df$kappa1[i] <- kappa1
+            prec0 = ifelse(muSpecified, n0/varSample[i], 0)
+            precData = nObs / varSample[i]
+            prec1 = prec0 + precData
+            v1 = 1/prec1
+            std1 = sqrt(v1)
+            m1 = m0*prec0/prec1 + yBar*precData/prec1
 
-            prec0 <- ifelse(muSpecified, n0/varSample[i], 0)
-            precData <- nObs / varSample[i]
-            prec1 <- prec0 + precData
-            v1 <- 1/prec1
-            std1 <- sqrt(v1)
-            m1 <- m0*prec0/prec1 + yBar*precData/prec1
-
-
-            temp.df$prec1[i] <- prec1
-            temp.df$v1[i] <- v1
-            temp.df$std[i] <- std1
-            temp.df$m1[i] <- m1
-
-            muSample[i] <- z[i]*std1 + m1
+            muSample[i] = z[i]*std1 + m1
         }
 
-        sigmaSample <- sqrt(varSample)
+        sigmaSample = sqrt(varSample)
 
-        oldPar<-par(mfrow = c(2,2))
+        oldPar = par(mfrow = c(2,2))
 
         plot(ts(muSample),main = "Time series plot of mu", ylab = expression(mu))
         plot(ts(varSample),main = "Time series plot of var"
@@ -128,16 +111,16 @@ normGibbs<-function(y, steps = 1000, type = 'ind', ...){
         plot(ts(sigmaSample), main = "Time series plot of sigma",
              ylab = expression(sigma))
         hist(muSample, main = "Histogram of mu", prob = TRUE)
-        mx<-mean(muSample)
-        sx<-sd(muSample)
-        bds<-mx+c(-5,5)*sx
-        xValues<-seq(bds[1],bds[2],length=200)
-        yValues<-dnorm(xValues,mx,sx)
+        mx = mean(muSample)
+        sx = sd(muSample)
+        bds = mx+c(-5,5)*sx
+        xValues=seq(bds[1],bds[2],length = 200)
+        yValues = dnorm(xValues,mx,sx)
         lines(xValues,yValues)
 
         par(oldPar)
 
-        results.df <- data.frame(mu = muSample, sig = sigmaSample, var = varSample)
+        results.df = data.frame(mu = muSample, sig = sigmaSample, var = varSample)
 
         describe(results.df)
         invisible(results.df)
@@ -146,17 +129,17 @@ normGibbs<-function(y, steps = 1000, type = 'ind', ...){
         ## The dots can carry priorMu which, if specified
         ## can be a single number m0 or a vector m0, n0
 
-        m0 <- 0
-        n0 <- 0
-        v0 <- 0
+        m0 = 0
+        n0 = 0
+        v0 = 0
 
-        muSpecified <- FALSE
+        muSpecified = FALSE
 
         if("priorMu" %in% names(dots)){
             if(length(dots$priorMu)==2){
-                m0 <- dots$priorMu[1]
-                n0 <- dots$priorMu[2]
-                muSpecified <- TRUE
+                m0 = dots$priorMu[1]
+                n0 = dots$priorMu[2]
+                muSpecified = TRUE
             }else{
                 stop("priorMu must contain a mean and an effective sample size")
             }
@@ -165,95 +148,76 @@ normGibbs<-function(y, steps = 1000, type = 'ind', ...){
         ## The dots can carry priorVar which, if specified
         ## must be a vector of length 2
 
-        s0 <- 0
-        kappa0 <- 0
-        varSpecified <- FALSE
+        s0 = 0
+        kappa0 = 0
+        varSpecified = FALSE
 
-        v0 <- 0
+        v0 = 0
 
         if("priorVar" %in% names(dots)){
             if(length(dots$priorVar)!=2)
                 stop("priorVar must have two elements, s0 and kappa0")
             else{
-                s0 <- dots$priorVar[1]
-                kappa0 <-dots$priorVar[2]
-                varSpecified <- TRUE
+                s0 = dots$priorVar[1]
+                kappa0 =dots$priorVar[2]
+                varSpecified = TRUE
             }
         }
 
-        nObs <- length(y)
-        yBar <- mean(y)
-        SSy <- sum((y-yBar)^2)
+        nObs = length(y)
+        yBar = mean(y)
+        SSy = sum((y-yBar)^2)
 
-        v0 <- ifelse(varSpecified, s0/rchisq(1,df = kappa0), SSy/(nObs-1))
+        v0 = ifelse(varSpecified, s0/rchisq(1,df = kappa0), SSy/(nObs-1))
 
-        kappa1 <- kappa0 + nObs
+        kappa1 = kappa0 + nObs
 
-        prec0 <- 0
-        mu0 <- yBar
+        prec0 = 0
+        mu0 = yBar
 
-        muSample <- rep(0, steps)
+        muSample = rep(0, steps)
 
         if(muSpecified){
-            prec0 <- n0/v0
-            mu0 <- m0 + rnorm(1)*sqrt(v0)
+            prec0 = n0/v0
+            mu0 = m0 + rnorm(1)*sqrt(v0)
         }else{
-            m0 <- 0
+            m0 = 0
         }
 
 
-        SSt <- sum((y-mu0)^2)
-        s1 <- s0 + SSt
+        SSt = sum((y-mu0)^2)
+        s1 = s0 + SSt
 
-        Chi <- rchisq(steps, df = kappa1)
+        Chi = rchisq(steps, df = kappa1)
 
-        varSample <- rep(0, steps)
-        temp.df<-data.frame(SSt = rep(0, steps), s1 = rep(0, steps), kappa1 = rep(0, steps),
-                            prec1 = rep(0, steps), v1 = rep(0, steps), m1 = rep(0, steps))
+        varSample = c(s1/Chi[1],rep(0,steps-1))
+        Z = rnorm(steps)
 
-        varSample[1] <- s1/Chi[1]
-        temp.df$SSt[1] <- SSt
-        temp.df$s1[1] <- s1
-        temp.df$kapp1[1] <- kappa1
+        prec0 = ifelse(muSpecified, n0/varSample[1], 0)
+        precData = nObs/varSample[1]
+        prec1 = prec0 + precData
+        v1 = 1/prec1
+        m1 = prec0/prec1*m0 + precData/prec1*yBar
 
-        Z <- rnorm(steps)
-
-        prec0 <- ifelse(muSpecified, n0/varSample[1], 0)
-        precData <- nObs/varSample[1]
-        prec1 <- prec0 + precData
-        v1 <- 1/prec1
-        m1 <- prec0/prec1*m0 + precData/prec1*yBar
-
-        temp.df$prec1[1] <- prec1
-        temp.df$v1[1] <- v1
-        temp.df$m1 <- m1
-
-        muSample[1]<-Z[1]*sqrt(v1)+m1
+        muSample[1] = Z[1]*sqrt(v1)+m1
 
         for(i in 2:steps){
-            SSt <- sum((y-muSample[i-1])^2)
-            s1 <- s0 + SSt
-            varSample[i] <- s1/Chi[i]
-            temp.df$SSt[i] <- SSt
-            temp.df$s1[i] <- s1
-            temp.df$kapp1[i] <- kappa1
+            SSt = sum((y-muSample[i-1])^2)
+            s1 = s0 + SSt
+            varSample[i] = s1/Chi[i]
 
-            prec0 <- ifelse(muSpecified, n0/varSample[i],0)
-            precData <- nObs/varSample[i]
-            prec1 <- prec0 + precData
-            v1 <- 1/prec1
-            m1 <- prec0/prec1*m0 + precData/prec1*yBar
+            prec0 = ifelse(muSpecified, n0/varSample[i],0)
+            precData = nObs/varSample[i]
+            prec1 = prec0 + precData
+            v1 = 1/prec1
+            m1 = prec0/prec1*m0 + precData/prec1*yBar
 
-            temp.df$prec1[i] <- prec1
-            temp.df$v1[i] <- v1
-            temp.df$m1 <- m1
-
-            muSample[i]<-Z[i]*sqrt(v1)+m1
+            muSample[i] = Z[i]*sqrt(v1)+m1
         }
 
-        sigmaSample <- sqrt(varSample)
+        sigmaSample = sqrt(varSample)
 
-        oldPar<-par(mfrow = c(2,2))
+        oldPar = par(mfrow = c(2,2))
 
         plot(ts(muSample),main = "Time series plot of mu", ylab = expression(mu))
         plot(ts(varSample),main = "Time series plot of var"
@@ -261,16 +225,16 @@ normGibbs<-function(y, steps = 1000, type = 'ind', ...){
         plot(ts(sigmaSample), main = "Time series plot of sigma",
              ylab = expression(sigma))
         hist(muSample, main = "Histogram of mu", prob = TRUE)
-        mx<-mean(muSample)
-        sx<-sd(muSample)
-        bds<-mx+c(-5,5)*sx
-        xValues<-seq(bds[1],bds[2],length=200)
-        yValues<-dnorm(xValues,mx,sx)
+        mx = mean(muSample)
+        sx = sd(muSample)
+        bds = mx+c(-5,5)*sx
+        xValues=seq(bds[1],bds[2],length = 200)
+        yValues = dnorm(xValues,mx,sx)
         lines(xValues,yValues)
 
         par(oldPar)
 
-        results.df <- data.frame(mu = muSample, sig = sigmaSample, var = varSample)
+        results.df = data.frame(mu = muSample, sig = sigmaSample, var = varSample)
 
         describe(results.df)
         invisible(results.df)
